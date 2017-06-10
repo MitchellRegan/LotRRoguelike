b6a5249149c4b8a5a6f915dfcf653323d667c6a4
Mitch Regan <mitchell.regan@digipen.edu>
2017-06-09 23:53:43 -0500

    - Brought over the Interpolator.cs class from other projects I've worked on. It will be used to calculate distribution curves for attacks - Created the Combat folder for scripts related to combat - Created the AttackList.cs component. This holds a list for all of the attacks that a character/weapon can use in combat and the stats associated with it - Added a function in the CombatManager.cs that is used to calculate damage using distribution curves and attack min/max damage values

--- LotRGame/Assets/Scripts/2Menus/Combat UI/CombatManager.cs
+++ LotRGame/Assets/Scripts/2Menus/Combat UI/CombatManager.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
+    //The mathematical interpolator that we'll use to find damage using min/max ranges
+    private Interpolator ourInterpolator;
+
+    //The list of all characters involved in this combat
+    [HideInInspector]
+    public List<Character> charactersInCombat;
+
+
 
 	// Use this for initialization
-	void Start () {
-		
+	private void Awake ()
+    {
+        this.ourInterpolator = new Interpolator();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    //Function called when one character attacks another. Returns the damage based on the mathematical distribution
+    public int CalculateDamage(int minDamage_, int maxDamage_, EaseType distribution_)
+    {
+        //If the max damage is 0, no damage can be dealt
+        if(maxDamage_ == 0)
+        {
+            return 0;
+        }
+
+        //Int to hold the damage that's returned. It's automatically set to the min
+        int totalDamage = minDamage_;
+        //Finding the difference between the min and max
+        int damageDiff = maxDamage_ - minDamage_;
+
+        //Setting the distribution type of the interpolator and resetting the progress
+        this.ourInterpolator.ease = distribution_;
+        this.ourInterpolator.ResetTime();
+        //Setting the interp to a random value from 0 - 1
+        this.ourInterpolator.AddTime(Random.Range(0, 1));
+
+        //Adding a weighted percentage of the difference to the total
+        totalDamage += damageDiff * Mathf.RoundToInt(this.ourInterpolator.GetProgress());
+
+        return totalDamage;
+    }
 }
---
+++ LotRGame/Assets/Scripts/Gameplay/Combat/AttackList.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackList : MonoBehaviour
+{
+    //The list of attacks that this character or weapon can perform
+    public List<AttackInfo> attacks;
+}
+
+
+//A class used in AttackList.cs and CombatManager.cs that describes different attacks
+[System.Serializable]
+public class AttackInfo
+{
+    //The displayed name of this attack
+    public string attackName = "";
+
+    //The type of action this attack requires
+    public enum AttackActionType { Standard, FullRound, Quick, Free };
+    public AttackActionType actionType = AttackActionType.Standard;
+
+    //The range of this attack in terms of spaces
+    public int attackRange = 1;
+
+    //The skill used for determining accuracy
+    public Weapon.WeaponType weaponSkillUsed = Weapon.WeaponType.Punching;
+
+    //The skill check to beat when using this attack
+    [Range(10, 100)]
+    public int hitDC = 50;
+
+    //The percent chance that this attack will crit
+    [Range(0, 90)]
+    public float critChance = 20;
+
+    //The damage multiplier applied when this weapon crits
+    public float critMultiplier = 2;
+    [Space(10)]
+
+
+    //The range of physical damage this attack can deal if it hits
+    public Vector2 physicalDamageRange = new Vector2(2, 12);
+    public EaseType physicalDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of fire damage this attack can deal if it hits
+    public Vector2 fireDamageRange = new Vector2(0, 0);
+    public EaseType fireDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of water damage this attack can deal if it hits
+    public Vector2 waterDamageRange = new Vector2(0, 0);
+    public EaseType waterDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of electric damage this attack can deal if it hits
+    public Vector2 electricDamageRange = new Vector2(0, 0);
+    public EaseType electricDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of wind damage this attack can deal if it hits
+    public Vector2 windDamageRange = new Vector2(0, 0);
+    public EaseType windDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of rock damage this attack can deal if it hits
+    public Vector2 rockDamageRange = new Vector2(0, 0);
+    public EaseType rockDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of light damage this attack can deal if it hits
+    public Vector2 lightDamageRange = new Vector2(0, 0);
+    public EaseType lightDamageDistribution = EaseType.Linear;
+    [Space(10)]
+
+    //The range of dark damage this attack can deal if it hits
+    public Vector2 darkDamageRange = new Vector2(0, 0);
+    public EaseType darkDamageDistribution = EaseType.Linear;
+}
