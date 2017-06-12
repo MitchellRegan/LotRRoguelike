34fec625919086af52f66cbeff26bd4910403cd8
Mitch Regan <mitchell.regan@digipen.edu>
2017-06-12 01:44:28 -0500

    - Created a test Enemy Encounter object prefab so I can start testing how encounters begin - Modified EnemyEncounter.cs so that it has a list of what enemies are spawned and what their spawn positions are when combat begins

--- LotRGame/Assets/Scripts/Gameplay/Combat/EnemyEncounter.cs
+++ LotRGame/Assets/Scripts/Gameplay/Combat/EnemyEncounter.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(Movement))]
 public class EnemyEncounter : MonoBehaviour
 {
+    //The list of all enemies that are spawned when combat begins
+    public List<EncounterEnemy> enemies;
+}
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+[System.Serializable]
+public class EncounterEnemy
+{
+    //The enemy character
+    public Character enemyCreature;
+
+    //Enums that determine what section of the combat grid this enemy starts in
+    public enum colPositionAreas { Front, Middle, Back, Random, SpecificPos };
+    public enum rowPositionAreas { Top, Middle, Bottom, Random, SpecificPos };
+    public colPositionAreas colArea = colPositionAreas.Random;
+    public rowPositionAreas rowArea = rowPositionAreas.Random;
+
+    //Ints for specific row/col positions. If negative, they're ignored and set to randomized
+    public int specificCol = -1;
+    public int specificRow = -1;
 }
