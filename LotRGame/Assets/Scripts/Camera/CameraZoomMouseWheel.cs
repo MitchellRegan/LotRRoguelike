40230f18e1d5ea969dd0dd0d2f2c388407c3ba35
Mitch Regan <mitchell.regan@digipen.edu>
2017-07-13 22:56:33 -0500

    - Modified the CameraZoomMouseWheel.cs script so that it scrolls out faster the further away it gets, rather than scrolling slower (like it was before) - Also made it so the CameraZoomMouseWheel.cs won't zoom in or out when the player's mouse is over a scroll rectangle

--- LotRGame/Assets/Scripts/Camera/CameraZoomMouseWheel.cs
+++ LotRGame/Assets/Scripts/Camera/CameraZoomMouseWheel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CameraZoomMouseWheel : MonoBehaviour
 {
@@ -11,12 +13,31 @@ public class CameraZoomMouseWheel : MonoBehaviour
     public Vector2 scrollSpeedMinMax = new Vector2(100, 250);
 
 
-
+    
     //Function called every frame
-	private void Update()
+    private void Update()
     {
+        //Raycasting from the mouse to find out what's under it
+        List<RaycastResult> results = new List<RaycastResult>();
+        PointerEventData pointerData = new PointerEventData(EventSystem.current);
+        pointerData.position = Input.mousePosition;
+        EventSystem.current.RaycastAll(pointerData, results);
+
+        //Looping through all of the objects hit by the raycast
+        if(results.Count > 0)
+        {
+            foreach(RaycastResult rayResult in results)
+            {
+                //If we find a scroll rectangle under the mouse's current position, we prevent the player from zooming
+                if(rayResult.gameObject.GetComponent<ScrollRect>())
+                {
+                    return;
+                }
+            }
+        }
+
         //Finding the correct amount to zoom in based on the camera's current distance
-        float distPercent = (this.transform.localPosition.z - this.zPosMinMax.x) / (this.zPosMinMax.y - this.zPosMinMax.x);
+        float distPercent = 1 - (this.transform.localPosition.z - this.zPosMinMax.x) / (this.zPosMinMax.y - this.zPosMinMax.x);
         float scrollSpeed = (distPercent * (this.scrollSpeedMinMax.y - this.scrollSpeedMinMax.x)) + this.scrollSpeedMinMax.x;
 
         //Moves this camera in and out based on the input from the mouse scroll wheel
