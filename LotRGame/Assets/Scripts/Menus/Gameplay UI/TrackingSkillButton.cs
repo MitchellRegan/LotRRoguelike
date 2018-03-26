b653062a7dec1269b87ca5be651d01a95d3b184b
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2018-03-26 02:32:10 -0500

    - Created the FishingSkillButton.cs, ForageSkillButton.cs, and TrackingSkillButton.cs scripts for the overworld UI so that the player can attempt survival rolls to find resources - Modified the GamePlay scene to include UI buttons for the Forage, Fish, and Tracking skill checks

---
+++ LotRGame/Assets/Scripts/Menus/Gameplay UI/TrackingSkillButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingSkillButton : MonoBehaviour
+{
+    //The number of hours that pass when this skill is used
+    [Range(0, 9)]
+    public int hoursPassed = 3;
+
+
+
+    //Function called externally from UI buttons to roll for tracking skills
+    public void TrackingSkillCheck()
+    {
+        //Telling the time panel how many hours will pass while tracking
+        TimePanelUI.globalReference.AdvanceTime(this.hoursPassed);
+
+        //Getting the reference to the tile that the player party is currently on
+        TileInfo playerTile = PartyGroup.group1.GetComponent<WASDOverworldMovement>().currentTile;
+
+        //Int to hold the highest skill roll
+        int highestRoll = 0;
+
+        //Looping through each player character in the party
+        for (int pc = 0; pc < PartyGroup.group1.charactersInParty.Count; ++pc)
+        {
+            //Making sure the current character slot isn't null
+            if (PartyGroup.group1.charactersInParty[pc] != null)
+            {
+                //Rolling to see what the current character's skill check is
+                int skillCheck = PartyGroup.group1.charactersInParty[pc].charSkills.survivalist;
+                skillCheck += PartyGroup.group1.charactersInParty[pc].charSkills.survivalistMod;
+                skillCheck += Random.Range(1, 100);
+
+                //If the current skill check is higher than the current highest, this check becomes the new highest
+                if (skillCheck > highestRoll)
+                {
+                    highestRoll = skillCheck;
+                }
+            }
+        }
+
+        Debug.Log("This is where we need to check tracking results. Highest roll: " + highestRoll);
+    }
+}
