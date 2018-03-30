7a4879521f2235b0813c060ea0d2446534effa2d
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2018-03-29 22:04:33 -0500

    - Modified the TrackingSkillButton.cs script so that it compares the player party's survival check against the tile's tracking encounter list and begins combat if they roll high enough. Like the other skill buttons, this is currently untested

--- LotRGame/Assets/Scripts/Menus/Gameplay UI/TrackingSkillButton.cs
+++ LotRGame/Assets/Scripts/Menus/Gameplay UI/TrackingSkillButton.cs
@@ -16,8 +16,8 @@ public class TrackingSkillButton : MonoBehaviour
         //Telling the time panel how many hours will pass while tracking
         TimePanelUI.globalReference.AdvanceTime(this.hoursPassed);
 
-        //Getting the reference to the tile that the player party is currently on
-        TileInfo playerTile = PartyGroup.group1.GetComponent<WASDOverworldMovement>().currentTile;
+        //Getting the reference to the tracking block of the tile that the player party is currently on
+        List<EncounterBlock> trackingEncounters = PartyGroup.group1.GetComponent<WASDOverworldMovement>().currentTile.getTrackingEncounters();
 
         //Int to hold the highest skill roll
         int highestRoll = 0;
@@ -42,5 +42,30 @@ public class TrackingSkillButton : MonoBehaviour
         }
 
         Debug.Log("This is where we need to check tracking results. Highest roll: " + highestRoll);
+
+        //Looping through this tile's tracking encounter blocks to see what the highest success is
+        int currentHighestIndex = -1;
+        for(int t = 0; t < trackingEncounters.Count; ++t)
+        {
+            //If the current encounter's skill check is less than or equal to the best roll
+            if(trackingEncounters[t].skillCheck <= highestRoll)
+            {
+                //If the current encounter's skill is higher than the current highest encounter's, it becomes the new highest
+                if(trackingEncounters[t].skillCheck > trackingEncounters[currentHighestIndex].skillCheck)
+                {
+                    currentHighestIndex = t;
+                }
+            }
+        }
+
+        //If the current highest index is greater than -1, the player has succeeded and we start an encounter
+        if(currentHighestIndex > -1)
+        {
+            //Getting the variables for the player party and tile to begin combat
+            LandType tileType = PartyGroup.group1.GetComponent<WASDOverworldMovement>().currentTile.type;
+            PartyGroup playerParty = PartyGroup.group1;
+            //Telling the combat manager to start the fight
+            CombatManager.globalReference.InitiateCombat(tileType, playerParty, trackingEncounters[currentHighestIndex].encounterEnemies);
+        }
     }
 }
