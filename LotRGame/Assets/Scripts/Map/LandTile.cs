dea9ddfaf36ff66974e9761d6aaa91d39b83c517
Mitch-PC\Mitch <mitchell.p.regan@gmail.com>
2018-05-12 01:00:57 -0500

    - Created the HexTileExtrude.obj and HexTileSubdiv.obj tile objects to test different models for the overworld - Modified the HexTile prefab to use the HexTileSubdiv.obj mesh. At the moment I think it looks the best - Modified LandTile.cs to make the HexTileSubdiv.obj verts to align with the nearby tiles (a lot more verts to deal with)

--- LotRGame/Assets/Scripts/Map/LandTile.cs
+++ LotRGame/Assets/Scripts/Map/LandTile.cs
@@ -172,8 +172,70 @@ public class LandTile : MonoBehaviour
         //Our object's global transform position
         Vector3 ourPos = this.transform.position;
 
-        //1 = East
+        //0-Center,  1-E inner,  2-SE inner,  3-SW inner,  4-SW outer,  5-W outer,  6-W inner
+        //7-NW inner,   8-NE inner,   9-E outer,   10-NE outer,   11-SE outer,   12-NW outer,   13-NW topFlat,   14-W topFlat,  15-W bottom,  16-NW bottom
+        //17-E topFlat,  18-NE topFlat,  19-NE bottom,  20-E bottom,  21-SW topFlat,  22-SE topflat,  23-SE bottom,  24-SW bottom,  25-SE topFlat
+        //26-E-topFlat,  27-E bottom,  28-SE bottom,  29-W topFlat,  30-SW topFlat,  31-SW bottom,  32-W bottom,  33-NE topFlat,  34-NW topFlat
+        //35-NW bottom,  36-NE bottom
+
+        //Inner: 1-E, 2-SE, 3-SW, 6-W, 7-NW, 8-NE
+        //Outer: 4-SW, 5-W, 9-E, 10-NE, 11-SE, 12-NW
+        //TopFlat: 13/34-NW,   14/29-W,   17/26-E,   18/33-NE,   21/30-SW,   22/25-SE
+        //Bottom: 15-W, 16-NW, 19-NE, 20-E, 23-SE, 24-SW, 27-E, 28-SE, 31-SW, 32-W, 35-NW, 36-NE
+
+        //Use this for Hex Tile EXTRUDE
+        //East (9, 17, 26)
         float eastHeight = this.FindTileHeight(TileDirections.Northeast) - this.tileReference.elevation;
+        eastHeight += this.FindTileHeight(TileDirections.Southeast) - this.tileReference.elevation;
+        eastHeight = eastHeight / 3;
+        vertices[9].Set(vertices[9].x, vertices[9].y + eastHeight, vertices[9].z);
+        vertices[17].Set(vertices[17].x, vertices[17].y + eastHeight, vertices[17].z);
+        vertices[26].Set(vertices[26].x, vertices[26].y + eastHeight, vertices[26].z);
+
+        //South East (11, 22, 25)
+        float southeastHeight = this.FindTileHeight(TileDirections.South) - this.tileReference.elevation;
+        southeastHeight += this.FindTileHeight(TileDirections.Southeast) - this.tileReference.elevation;
+        southeastHeight = southeastHeight / 3;
+        vertices[11].Set(vertices[11].x, vertices[11].y + southeastHeight, vertices[11].z);
+        vertices[22].Set(vertices[22].x, vertices[22].y + southeastHeight, vertices[22].z);
+        vertices[25].Set(vertices[25].x, vertices[25].y + southeastHeight, vertices[25].z);
+
+        //South West (4, 21, 30)
+        float southwestHeight = this.FindTileHeight(TileDirections.South) - this.tileReference.elevation;
+        southwestHeight += this.FindTileHeight(TileDirections.Southwest) - this.tileReference.elevation;
+        southwestHeight = southwestHeight / 3;
+        vertices[4].Set(vertices[4].x, vertices[4].y + southwestHeight, vertices[4].z);
+        vertices[21].Set(vertices[21].x, vertices[21].y + southwestHeight, vertices[21].z);
+        vertices[30].Set(vertices[30].x, vertices[30].y + southwestHeight, vertices[30].z);
+
+        //North East (10, 18, 33)
+        float northeastHeight = this.FindTileHeight(TileDirections.North) - this.tileReference.elevation;
+        northeastHeight += this.FindTileHeight(TileDirections.Northeast) - this.tileReference.elevation;
+        northeastHeight = northeastHeight / 3;
+        vertices[10].Set(vertices[10].x, vertices[10].y + northeastHeight, vertices[10].z);
+        vertices[18].Set(vertices[18].x, vertices[18].y + northeastHeight, vertices[18].z);
+        vertices[33].Set(vertices[33].x, vertices[33].y + northeastHeight, vertices[33].z);
+
+        //North West (12, 13, 34)
+        float northwestHeight = this.FindTileHeight(TileDirections.North) - this.tileReference.elevation;
+        northwestHeight += this.FindTileHeight(TileDirections.Northwest) - this.tileReference.elevation;
+        northwestHeight = northwestHeight / 3;
+        vertices[12].Set(vertices[12].x, vertices[12].y + northwestHeight, vertices[12].z);
+        vertices[13].Set(vertices[13].x, vertices[13].y + northwestHeight, vertices[13].z);
+        vertices[34].Set(vertices[34].x, vertices[34].y + northwestHeight, vertices[34].z);
+
+        //West (5, 14, 29)
+        float westHeight = this.FindTileHeight(TileDirections.Northwest) - this.tileReference.elevation;
+        westHeight += this.FindTileHeight(TileDirections.Southwest) - this.tileReference.elevation;
+        westHeight = westHeight / 3;
+        vertices[5].Set(vertices[5].x, vertices[5].y + westHeight, vertices[5].z);
+        vertices[14].Set(vertices[14].x, vertices[14].y + westHeight, vertices[14].z);
+        vertices[29].Set(vertices[29].x, vertices[29].y + westHeight, vertices[29].z);
+
+
+        //Use this for the Hex Tile FLAT mesh
+        //1 = East
+        /*float eastHeight = this.FindTileHeight(TileDirections.Northeast) - this.tileReference.elevation;
         eastHeight += this.FindTileHeight(TileDirections.Southeast) - this.tileReference.elevation;
         eastHeight = eastHeight / 3;
         vertices[1].Set(vertices[1].x, vertices[1].y + eastHeight, vertices[1].z);
@@ -206,9 +268,9 @@ public class LandTile : MonoBehaviour
         float westHeight = this.FindTileHeight(TileDirections.Northwest) - this.tileReference.elevation;
         westHeight += this.FindTileHeight(TileDirections.Southwest) - this.tileReference.elevation;
         westHeight = westHeight / 3;
-        vertices[6].Set(vertices[6].x, vertices[6].y + westHeight, vertices[6].z);
-        
-        
+        vertices[6].Set(vertices[6].x, vertices[6].y + westHeight, vertices[6].z);*/
+
+
         //Setting the mesh's verts to the changed array
         ourMesh.mesh.vertices = vertices;
     }
